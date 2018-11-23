f5e9c54326b9346cb1a7343561acbdbc74865d4a
LeonardoMonte <leopk97@hotmail.com>
2018-11-22 23:38:56 -0300

    Initial commit

---
+++ app/src/main/java/com/example/auricelia/reshipi/ActAvaliarReceita.java
@@ -0,0 +1,13 @@
+package com.example.auricelia.reshipi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActAvaliarReceita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_avaliar_receita);
+    }
+}
