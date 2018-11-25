71b8a4bff8c0ed0c4d0d282e590ee89b7cbb7074
LeonardoMonte <leopk97@hotmail.com>
2018-11-25 19:38:35 -0300

    Depois arrumo mais

--- app/src/main/java/com/example/auricelia/reshipi/BuscarPorIngred.kt
+++ app/src/main/java/com/example/auricelia/reshipi/BuscarPorIngred.kt
@@ -1,29 +1,32 @@
 package com.example.auricelia.reshipi
 
-import algoritmosIA.KNN
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_buscar_por_ingred.*
-import kotlin.collections.ArrayList
 
 class BuscarPorIngred : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buscar_por_ingred)
 
+        val myDataset = arrayOf("Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo"
+        ,"Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo"
+        ,"Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo"
+        ,"Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo"
+        ,"Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo","Leonardo")
 
-        val arr = arrayListOf<String>()
-        arr.add("Leonardo")
-        arr.add("Marcos")
-        arr.add("Fernando")
 
-        val file = assets.open("reshipejavafile3.txt")
-       val array = KNN.getInstancia(file).listarIngredientesCadastrados()
-       val mAdapter = CustomAdapter(this,arr)
-        RecView1.adapter = mAdapter
-
+        val viewAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, myDataset)
+        ListView1.adapter = viewAdapter
 
     }
 }
+
+
+
