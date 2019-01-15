08bad9fea89b00ea2d21175cacfa820d5e487e76
LeonardoMonte <leopk97@hotmail.com>
2019-01-15 02:47:46 -0300

    3

--- app/src/main/java/com/example/auricelia/reshipi/ActCadastro.kt
+++ app/src/main/java/com/example/auricelia/reshipi/ActCadastro.kt
@@ -5,13 +5,27 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.act_cadastro.*
 
 class ActCadastro : AppCompatActivity() {
 
+    var database : FirebaseDatabase? = null
+    var usuarios : DatabaseReference? = null
+    var mAuth: FirebaseAuth? = null
+    var mAuthListener: FirebaseAuth.AuthStateListener? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_cadastro)
+
+        database = FirebaseDatabase.getInstance()
+        usuarios = database!!.getReference("Usuarios")
+        mAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {  }
 
         buttonCadastrarX.setOnClickListener {
 
@@ -30,6 +44,53 @@ class ActCadastro : AppCompatActivity() {
             }
             else
             {
+
+                var usuario = Usuario(nomeET.text.toString(), cursoSpinner.selectedItem.toString(),
+                        campusSpinner.selectedItem.toString(), loginET.text.toString(), senhaET.text.toString())
+                usuario.EncodeString()
+
+                usuarios!!.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (verificarDados(usuario)){
+                            if (usuario.senha.length < 6){
+                                Toast.makeText(applicationContext, R.string.senha_pequena,
+                                        Toast.LENGTH_SHORT).show()
+                            }else{
+                                if (dataSnapshot.child(usuario.email).exists()){
+                                    Toast.makeText(applicationContext, R.string.email_cadastrado,
+                                            Toast.LENGTH_SHORT).show()
+                                }else{
+                                    usuario.DecodeString()
+                                    mAuth!!.createUserWithEmailAndPassword(usuario.email,usuario.senha)
+
+                                            .addOnCompleteListener { task ->
+                                                if (task.isSuccessful) {
+                                                    usuario.EncodeString()
+                                                    usuarios!!.child(usuario.email).setValue(usuario)
+                                                    Toast.makeText(applicationContext,
+                                                            R.string.usuario_criado, Toast.LENGTH_SHORT).show()
+                                                    val intent = Intent(applicationContext, Main2Activity::class.java)
+                                                    startActivity(intent)
+                                                    finish()
+                                                }
+                                            }
+
+                                            .addOnFailureListener { exception ->
+                                                if (exception != null) {
+                                                    Toast.makeText(applicationContext,
+                                                            exception.localizedMessage, Toast.LENGTH_LONG).show()
+                                                }
+                                            }
+                                }
+                            }
+                        }
+                    }
+                })
+
 
                 Toast.makeText(this, "Cadastro Realizado com sucesso", Toast.LENGTH_SHORT).show()
                 val i = Intent(this, MainActivity::class.java)
--- app/src/main/java/com/example/auricelia/reshipi/MainActivity.kt
+++ app/src/main/java/com/example/auricelia/reshipi/MainActivity.kt
@@ -5,10 +5,21 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.inicialscreen.*
 import kotlinx.android.synthetic.main.loginscreen.*
 
 class MainActivity : AppCompatActivity() {
+
+    var database : FirebaseDatabase? = null
+    var usuarios : DatabaseReference? = null
+    var mAuth: FirebaseAuth? = null
+    var mAuthListener: FirebaseAuth.AuthStateListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +28,16 @@ class MainActivity : AppCompatActivity() {
         val SDK_INT = android.os.Build.VERSION.SDK_INT
         if (SDK_INT > 8)
         {
-
             val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
             StrictMode.setThreadPolicy(policy)
             Reporeceitas.getInstancia()
 
         }
+
+        database = FirebaseDatabase.getInstance()
+        usuarios = database!!.getReference("Usuarios")
+        mAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {  }
 
         btnInicial.setOnClickListener {
 
@@ -30,6 +45,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(i)
         }
 
-
     }
+
+
 }
